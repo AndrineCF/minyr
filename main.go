5807de36f7b65f0569ec75ecdb676bc47fc3b6f8
Andrine Celine Flatby <andrine.celine.flatby@gmail.com>
2023-03-26 20:44:48 +0000

    startet med main og lese fil

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"log"
+)
 
 func main() {
+	//Open fil
+	src, err := os.Open("kjevik-temp-celsius-20220318-20230318.csv")
+
+	//check if there was error when open the file
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	//closing the fill when finished
+	defer src.Close()
+
+	log.Println(src)
+
 	fmt.Println("test")
 }
