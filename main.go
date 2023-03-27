296d5dba7390ffc7bf0fa0a64c4c7e5657de42bd
andrineCF <andrine.celine.flatby@gmail.com>
2023-03-27 09:27:40 +0000

    begynt paa lese fil

--- main.go
+++ main.go
@@ -8,13 +8,17 @@ import (
 )
 
 func main() {
+	// Open the main fil
+	filCel, err = os.OpenFile("kjvik-temp-celsius-20220318-20230318.csv", os.O_RDONLY, 0444)
+	scannerCel := bufio.NewScanner(filCel)
+	
 	//reading input from user
 	var input string
-	scanner := bufio.NewScanner(os.Stdin)
+	scannerInput := bufio.NewScanner(os.Stdin)
 
 	// loop to the user quit the program
-	for scanner.Scan() {
-		input = scanner.Text()
+	for scannerInput.Scan() {
+		input = scannerInput.Text()
 		
 		if input == "q" || input == "exit" {
 			fmt.Println("exit")
@@ -22,28 +26,25 @@ func main() {
 		} else if input == "convert" || input == "konvert" {
 			fmt.Println("Konverterer all maalingene gitt i grader Celsius til grader Fahrenheit")
 			// trying opening fil
-			fileConv, errConv := os.OpenFile("kjevik-temp-celsius-20220318-20230318", os.O_RDWR|os.O_CREATE, 0755)
+			fileConv, errConv := os.OpenFile("kjevik-temp-celsius-20220318-20230318.csv", os.O_RDWR|os.O_CREATE, 0755)
 			scannerConvFile := bufio.NewScanner(fileConv)
 			
 			// Check if the fil is empty
-			if len(scannerConvFile.Text()) == 0) {
-
-				for scannerConvFile.Scan() {
-					fmt.Println("for loop")
-					line := scanner.Text()
-					fmt.Println(line)
+			if len(scannerConvFile.Text()) == 0 {
+				for scannerCel.Scan() {
+					fmt.Println(scannerCel.Text())
 				}
 			} else {
 				fmt.Println("Filen finnes allerede. Vil du generere fil paa nytt?")
-				_ := bufio.NewScanner(os.Stdin)
+				scannerConv := bufio.NewScanner(os.Stdin)
 				
-				//for scannerConv.Scan() {
-				//	if scannerConv.Text() == "j"|| scannerconv.Text() == "ja"  {
-				//		fmt.Println("La oss skrive over den filen!")
-				//	} else if scannerConv.Text() == "n" || scannerConv.Text() == "nei" {
-				//		break
-				//	}
-				//}
+				for scannerConv.Scan() {
+					if scannerConv.Text() == "j"|| scannerConv.Text() == "ja"  {
+						fmt.Println("La oss skrive over den filen!")
+					} else if scannerConv.Text() == "n" || scannerConv.Text() == "nei" {
+						break
+					}
+				}
 			}
 			
 			//check if there errors
@@ -51,6 +52,8 @@ func main() {
 				log.Fatal(errConv)
 			}
 
+			//Closed the file
+			fileConv.Closed()
 		} else {
 			fmt.Println("Ikke gylding alternativ, venligst prove igjen")
 		}
