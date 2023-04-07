4dbacd75c08caa225df9becd9bd365a557c65397
Andrine Celine Flatby <andrine.celine.flatby@gmail.com>
2023-04-07 22:01:27 +0000

    skriv test for konverting

--- yr/yr_test.go
+++ yr/yr_test.go
@@ -25,3 +25,26 @@ func TestCountLines(t *testing.T) {
 		}
 	}
 }
+
+//Testing the conversion from celsius to fahrenheit
+func TestConvertCelsiusToFahrenheit(t *testing.T) {
+	type test struct {
+		input string
+		want string
+	}
+
+	tests :=[]test {
+		{input:"Kjevik;SN39040;18.03.2022 01:50;6",  want:"Kjevik;SN39040;18.03.2022 01:50;42.8"},
+		{input:"Kjevik;SN39040;07.03.2023 18:20;0", want:"Kjevik;SN39040;07.03.2023 18:20;32.0"},
+		{input:"Kjevik;SN39040;08.03.2023 02:20;-11", want:"Kjevik;SN39040;08.03.2023 02:20;12.2"},
+		{input:"Data er gyldig per 18.03.2023 (CC BY 4.0), Meteorologisk institutt (MET);;;",
+		want:"Data er basert p   gyldig data (per 18.03.2023) (CC BY 4.0) fra Meteorologisk institutt (MET);endringen er gjort av Andrine Celine Flatby;;;"},
+	}
+
+	for _, tc :range tests {
+		got :=ConvertCelsiusToFahrenheit(tc.input)
+		if !reflect.DeepEqual(tc.want, got) {
+                        t.Errorf("expected: %v, got: %v", tc.want, got)
+                }
+        }
+}
